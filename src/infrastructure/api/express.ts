39816394ef1da8da1a5e5502136d5ba336737575
Everton Goncalves <everton.goncalves.89@gmail.com>
2024-09-01 16:43:34 -0300

    feat: invoice api

--- src/infrastructure/api/express.ts
+++ src/infrastructure/api/express.ts
@@ -2,6 +2,7 @@ import express, { Express } from 'express';
 import { clientRoute } from './routes/client.route';
 import { productRoute } from './routes/product.route';
 import { checkoutRoute } from './routes/checkout.route';
+import { invoiceRoute } from './routes/invoice.route';
 
 export const app: Express = express();
 app.use(express.json());
@@ -9,3 +10,4 @@ app.use(express.json());
 app.use('/clients', clientRoute);
 app.use('/products', productRoute);
 app.use('/checkout', checkoutRoute);
+app.use('/invoices', invoiceRoute);
